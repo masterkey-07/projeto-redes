91f34f9552dc9a31f6ffd11b3cdd05e7da02283a
Victor Jorge Carvalho Chaves <victorchaves03@gmail.com>
2023-09-11 11:50:19 -0300

    initial commit

--- target/debug/deps/v2-2602f94ec2b4dfe6.d
+++
@@ -1,5 +0,0 @@
-/home/vjchave/Repositories/Java/projeto-redes/v2/target/debug/deps/v2-2602f94ec2b4dfe6.rmeta: src/main.rs
-
-/home/vjchave/Repositories/Java/projeto-redes/v2/target/debug/deps/v2-2602f94ec2b4dfe6.d: src/main.rs
-
-src/main.rs:
--- target/debug/v2.d
+++
@@ -1,1 +0,0 @@
-/home/vjchave/Repositories/Java/projeto-redes/v2/target/debug/v2: /home/vjchave/Repositories/Java/projeto-redes/v2/src/main.rs
